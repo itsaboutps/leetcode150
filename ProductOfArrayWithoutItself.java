b5096e128a78cb35ea87f1f17da0f209e667f397
Prashant Singh <331865@NTTDATA.COM>
2025-02-19 02:55:48 +0530

    Product of Array without Itself.

---
+++ ProductOfArrayWithoutItself.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class ProductOfArrayWithoutItself {
+    public static void main(String[] args) {
+        System.out.println("Product of Array without itself.");
+        int[] nums = { 1, 2, 3, 4 };
+        fn(nums);
+    }
+
+    private static int[] fn(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0,left=1; i < n; i++) {
+            ans[i] = left;
+            left *= nums[i];
+        }
+        System.out.println(Arrays.toString(ans));
+        for (int i = n-1,right=1; i >= 0; i--) {
+            ans[i] *=right;
+            right *=nums[i];
+        }
+        System.out.println(Arrays.toString(ans));
+        return ans;
+
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'fn'");
+
+    }
+}
