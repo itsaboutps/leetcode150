192aa4d8f596e77b0b8fe6a7e579eb3f25efdacc
Prashant Singh <331865@NTTDATA.COM>
2025-02-24 22:48:50 +0530

    Valid Parenthesis

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,44 @@
+package Stack;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        System.out.println("Valid Parentheses:: ");
+        String s = "([])";
+        System.out.print(optimised(s));
+        // System.out.print(myApproach(s));
+
+    }
+
+    private static char[] myApproach(String s) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'myApproach'");
+    }
+
+    private static boolean optimised(String s) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        java.util.Map<Character, Character> closeToOpen = new java.util.HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
+                    stack.pop();
+
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
