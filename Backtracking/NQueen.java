f3b9a0067cfa1783a4867006665ab3ea33d4c143
itsaboutps <psprashantsingh91@gmail.com>
2025-03-14 02:08:34 +0530

    Backtracking | Decision Tree

---
+++ Backtracking/NQueen.java
@@ -0,0 +1,64 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NQueen {
+    public static void main(String[] args) {
+        System.out.println("NQueen");
+        Solution solution = new Solution();
+        System.out.println(solution.solveNQueens(4));
+    }
+}
+
+class Solution {
+    Set<Integer> col = new HashSet<>();
+    Set<Integer> postDiag = new HashSet<>();
+    Set<Integer> negDiag = new HashSet<>();
+    List<List<String>> res = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+
+        backtrack(0, n, board);
+        return res;
+    }
+
+    private void backtrack(int r, int n, char[][] board) {
+        if (r == n) {
+            // hitting the base condition
+            List<String> copy = new ArrayList<>();
+            for (char[] row : board) {
+                copy.add(new String(row));
+            }
+            res.add(copy);
+            return;
+        }
+
+        for (int c = 0; c < n; c++) {
+            if (col.contains(c) || postDiag.contains(r + c) || negDiag.contains(r - c)) {
+                continue;
+                // skipping that column
+            }
+            col.add(c);
+            postDiag.add(r + c);
+            negDiag.add(r - c);
+            board[r][c] = 'Q';
+
+            backtrack(r + 1, n, board);
+
+            col.remove(c);
+            postDiag.remove(r+c);
+            negDiag.remove(r-c);
+            board[r][c] = '.';
+
+
+        }
+    }
+}
---
+++ Backtracking/Subsets.java
@@ -0,0 +1,57 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Backtracking/image.png
+public class Subsets {
+    public static void main(String[] args) {
+        System.out.println("Subsets");
+        int[] nums = { 1, 2, 3 };
+        SolutionBacktracking backtracking = new SolutionBacktracking();
+        System.out.println(backtracking.subsets(nums));
+    }
+}
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+
+        for (int num : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(res.get(i));
+                subset.add(num);
+                res.add(subset);
+            }
+        }
+        return res;
+    }
+}
+
+class SolutionBacktracking {
+    private List<List<Integer>> ans = new ArrayList<>();
+    private List<Integer> t = new ArrayList<>();
+    private int[] nums;
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        this.nums = nums;
+        dfs(0);
+        return ans;
+
+    }
+
+    private void dfs(int i) {
+         if (i == nums.length) {
+            ans.add(new ArrayList<>(t));
+            return;
+        }
+
+        dfs(i + 1);
+        t.add(nums[i]);
+        dfs(i + 1);
+        t.remove(t.size() - 1);
+    }
+}
