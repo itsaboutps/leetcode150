1498a11c4bef0fb163eb3161ce61b535feb821b7
itsaboutps <psprashantsingh91@gmail.com>
2025-03-18 04:30:32 +0530

    Brute Force DFS ==> Memoization ==> DP

--- DP/HouseRobberII.java
+++ DP/HouseRobberII.java
@@ -74,23 +74,25 @@ class HouseRobberIIMemoization {
 
 class HouseRobberIIUsingDP1 {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
-            return nums[0];
-        }
-        return Math.max(rob(nums, 0, n - 2),
-                        rob(nums, 1, n - 1));
+        if (nums.length == 1) return nums[0];
+
+        return Math.max(helper(Arrays.copyOfRange(nums, 1, nums.length)),
+                        helper(Arrays.copyOfRange(nums, 0, nums.length - 1)));
     }
 
-    private int rob(int[] nums, int start, int end) {
-        int n = nums.length;
-        int[] dp = new int[n + 1];
-        dp[start] = 0;
-        dp[start + 1] = nums[start];
-        for (int i = start + 2; i <= end + 1; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+    private int helper(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
         }
-        return dp[end + 1];
+
+        return dp[nums.length - 1];
     }
 }
 
@@ -99,18 +101,17 @@ class HouseRobberIIUsingDP1 {
 class HouseRobberIIUsingDP2 {
     public int rob(int[] nums) {
         return Math.max(nums[0], 
-        Math.max(houseRobberI(Arrays.copyOfRange(nums, 1, nums.length)), 
-        houseRobberI(Arrays.copyOfRange(nums, 0, nums.length - 1))));
+               Math.max(helper(Arrays.copyOfRange(nums, 1, nums.length)), 
+               helper(Arrays.copyOfRange(nums, 0, nums.length - 1))));
     }
 
-    private int houseRobberI(int[] nums) {
-        int rob1 =0;
-        int rob2 = 0;
+    private int helper(int[] nums) {
+        int rob1 = 0, rob2 = 0;
 
-        for(int num : nums){
-            int temp = Math.max((rob1+ num), rob2);
+        for (int num : nums) {
+            int newRob = Math.max(rob1 + num, rob2);
             rob1 = rob2;
-            rob2 = temp;
+            rob2 = newRob;
         }
         return rob2;
     }
